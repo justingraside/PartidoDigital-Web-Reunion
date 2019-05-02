176d75d5726b84b62067b5526ab0e69b65b9412f
Leo Giovanetti <hello@leog.me>
2019-05-02 10:21:27 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ var second = 1000,
     day = hour * 24,
     hoy = new Date(),
     diaReunion = 3,
-    fechaReunion = new Date("May 2 2019"),
+    fechaReunion = new Date("May 2 2019 20:00:00 GMT-0300"),
     horaReunion = { h: 20, m: 00 };
 
 var meses = [ "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
